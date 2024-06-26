736057da85ef2a46f8832392efe1630e90b19bd5
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-06-26 22:41:06 +0100

    setting up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <h2>My Portfolio</h2> -->
+  <home-page></home-page>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from "./components/HomePage.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HomePage,
+  },
+};
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
