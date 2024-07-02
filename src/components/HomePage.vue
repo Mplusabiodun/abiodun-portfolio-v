15a6eef536f6505df26046d6093f41150781a72a
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-07-02 22:51:43 +0100

    landingpage html

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,99 @@
 <template>
   <div class="pcontainer">
-    <div class="fcontainer"></div>
-    <div class="scontainer"></div>
+    <div class="fcontainer">
+      <div>
+        <p>M. ABIODUN FATAI</p>
+        <hr />
+        <p>PORTFOLIO 2024</p>
+      </div>
+      <div>
+        <p>FRONT-END WEB DEVELOPER</p>
+        <img src="#" alt="Abiodun Fatai" />
+      </div>
+      <h2>I'M GLAD YOU'RE HERE, WELCOME TO MY PORTFOLIO</h2>
+      <p>I'M ABIODUN FATAI</p>
+      <p>
+        A National Diploma Distinction holder and currently CIVIL ENGINEERING
+        student at FEDERAL UNIVERSITY OF TECHNOLOGY MINNA, NIGER STATE. A
+        Frontend developer with great enthusiasm for building user centric
+        software products. I started this journey to explore my passion for
+        writing codes and solving complex software problems. Trust me it was
+        indeed amazing writing my first HTML code ('Hello world') and being able
+        to display on the screen.
+      </p>
+    </div>
+    <!-- I'm a Software Engineer (Frontend Dev.), finding my way into the world of
+    Tech. A National Diploma Distinction holder in Civil Engineering and
+    currectly a Civil Engineering student at Federal University of Technology
+    Minna, Niger State, Nigeria (Class of 2022). Always ready to learn new
+    things (so i can be better than i was yesterday)... Here is just the
+    beginning, Check me out tomorrow and cheers to great years in tech 😊 Xoxo
+    Here's a quick summary about me: 😊 Pronouns: He/him 💡 Fun fact: I'm
+    currently studying at AltSchool Africa School of Software Engineering Class
+    of 2022. And from my mentor Damola Badmus, also a member at Segsalerty. 🌱
+    I’m currently learning JavaScript, Reactjs, Vue and Python. 😊 I’m looking
+    for help with open source projects, hackathons, internships, and entry-level
+    opportunities. 💼 Job interests: Software Engineer, Front Engineer, or UI
+    Engineer (Intern or Junior level). 📫 You can view my resume, WhatsApp
+    +2348025202138 and contact me by emailing fataimonsuru2020@gmail.com.
+    In-between kudos to my mentor Damola Badmus -->
+
+    <!-- I’m a National Diploma Distinction holder and currently CIVIL ENGINEERING
+    student at FEDERAL UNIVERSITY OF TECHNOLOGY MINNA, NIGER STATE. A Frontend
+    developer with great enthusiasm for building user centric software products.
+    I started this journey to explore my passion for writing codes and solving
+    complex software problems. Trust me is ineed amazing wrinting my first HTML 
+    code ('Hello world') and being able to display it on the screen. I take pride
+    in providing the best in my career path as I was recently recognized as the 
+    best student in my cohort at the AltSchool Africa where I was trained as a 
+    software engineer (HTML, CSS, Tailwind, JavaScript and currently on Frameworks - React.js,
+    Vue.js...........) also experienced in contributing to open source project
+    and technical writing. -->
+
+    <div class="scontainer">
+      <div>
+        <h3>ADDRESS</h3>
+        <p>No.6 Fatai's Lodge, Idofin, Igboora, Oyo State. Nigeria</p>
+      </div>
+      <div>
+        <h3>EDUCATION</h3>
+        <p></p>
+      </div>
+      <div>
+        <h3>SKILLED</h3>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>GIT VC</li>
+        <li>JAVASCRIPT</li>
+        <li>VUE</li>
+        <li>REACT</li>
+      </div>
+      <div>
+        <h3>PROJECTS</h3>
+        <li></li>
+      </div>
+      <div>
+        <h3>CONTACT</h3>
+        <p>fataimonsuru2020@gmail.com</p>
+        <p>09063268232</p>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
 .pcontainer {
-  /* background: url("@/assets/about.jpg");
+  background: url("@/assets/emptypaper.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   width: 100%;
-  height: 100vh; */
-  display: flex;
-  justify-content: center;
+  /* height: 100vh; */
+  height: 50rem;
+  /* margin-top: 1rem; */
+  /* display: flex;
+  justify-content: center; */
 }
-.fcontainer,
+/* .fcontainer,
 .scontainer {
   width: 40%;
   height: 35rem;
@@ -24,12 +102,11 @@
   margin: 3rem 0;
 }
 .fcontainer {
-  /* margin-right: 0.1px; */
   border-top-right-radius: 15px;
   border-bottom-right-radius: 15px;
 }
 .scontainer {
   border-top-left-radius: 15px;
   border-bottom-left-radius: 15px;
-}
+} */
 </style>
